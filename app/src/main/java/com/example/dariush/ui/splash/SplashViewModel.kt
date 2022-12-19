719521cc73bfd908fd6d2553a5005cfae088a89d
Darush <darushfaqiri@gmail.com>
2022-12-19 14:16:27 +0300

    test: Given splash screen is opened, Then fetch weather data for the default location

--- app/src/main/java/com/example/dariush/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/dariush/ui/splash/SplashViewModel.kt
@@ -1,5 +1,50 @@
 package com.example.dariush.ui.splash
 
-class SplashViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.dariush.data.remote.WeatherRepository
+import com.example.dariush.data.succeeded
+import com.example.dariush.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@OptIn(ObsoleteCoroutinesApi::class)
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+  private val weatherRepository: WeatherRepository
+) : BaseViewModel<SplashViewModel.Event, SplashViewModel.UiState>() {
+
+  data class UiState(
+    val isLoading: Boolean = false
+  )
+
+  sealed class Event {}
+
+  override val _uiState = MutableLiveData(UiState(isLoading = true))
+
+  init {
+
+    viewModelScope.launch {
+      val result = weatherRepository.fetchLocationData("Istanbul")
+      when (result.succeeded) {
+        true  -> {
+          TODO("unimplemented")
+        }
+        false -> {
+          TODO("unimplemented")
+        }
+      }
+    }
+
+    viewModelScope.launch {
+      eventsFlow.collect { event ->
+        when (event) {
+          else -> {}
+        }
+      }
+    }
+  }
 
 }
--- app/src/test/java/com/example/dariush/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/example/dariush/ui/splash/SplashViewModelTest.kt
@@ -1,18 +1,32 @@
 package com.example.dariush.ui.splash
 
+import com.example.dariush.base.MainCoroutineRule
 import com.example.dariush.data.remote.WeatherRepository
+import io.mockk.MockKAnnotations
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SplashViewModelTest {
 
+  @OptIn(ExperimentalCoroutinesApi::class)
+  @get:Rule
+  val mainCoroutineRule = MainCoroutineRule()
+
   @MockK(relaxed = true)
   lateinit var mockWeatherRepository: WeatherRepository
 
+  @Before
+  fun setUp() {
+    MockKAnnotations.init(this)
+  }
+
   @Test
   fun `Given splash screen is opened, Then fetch weather data for the default location`() {
-    val vm = SplashViewModel()
+    val vm = SplashViewModel(mockWeatherRepository)
     val defaultLocation = "Istanbul"
 
     coVerify {
@@ -22,12 +36,12 @@ class SplashViewModelTest {
 
   @Test
   fun `Given weather data is being fetched, When fetch is successful, Then navigate to the search screen`() {
-
+    TODO("unimplemented")
   }
 
   @Test
   fun `Given weather data is fetching, When fetch fails, Then show error and exit the app`() {
-
+    TODO("unimplemented")
   }
 
 }
