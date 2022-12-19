cc733da22090e04356b74266579c0c09ced99cd4
Darush <darushfaqiri@gmail.com>
2022-12-19 14:33:23 +0300

    test: Given weather data is being fetched, When fetch is successful, Then navigate to the search screen

--- app/src/main/java/com/example/dariush/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/dariush/ui/splash/SplashViewModel.kt
@@ -2,8 +2,9 @@ package com.example.dariush.ui.splash
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.dariush.data.model.WeatherResponseModel
 import com.example.dariush.data.remote.WeatherRepository
-import com.example.dariush.data.succeeded
+import com.example.dariush.data.Result
 import com.example.dariush.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -17,23 +18,29 @@ class SplashViewModel @Inject constructor(
 ) : BaseViewModel<SplashViewModel.Event, SplashViewModel.UiState>() {
 
   data class UiState(
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val defaultWeatherData: WeatherResponseModel? = null
   )
 
-  sealed class Event {}
+  sealed class Event {
+    object NavigateToSearchScreen : Event()
+  }
 
   override val _uiState = MutableLiveData(UiState(isLoading = true))
 
   init {
 
     viewModelScope.launch {
-      val result = weatherRepository.fetchLocationData("Istanbul")
-      when (result.succeeded) {
-        true  -> {
+      when (val result = weatherRepository.fetchLocationData("Istanbul")) {
+        is Result.Success -> {
+          _uiState.value = _uiState.value?.copy(isLoading = false, defaultWeatherData = result.data)
+          sendEvent(Event.NavigateToSearchScreen)
+        }
+        is Result.Error   -> {
           TODO("unimplemented")
         }
-        false -> {
-          TODO("unimplemented")
+        Result.Loading    -> {
+          _uiState.value = _uiState.value?.copy(isLoading = true)
         }
       }
     }
--- app/src/test/java/com/example/dariush/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/example/dariush/ui/splash/SplashViewModelTest.kt
@@ -1,20 +1,34 @@
 package com.example.dariush.ui.splash
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.dariush.base.MainCoroutineRule
+import com.example.dariush.data.Result
+import com.example.dariush.data.model.WeatherResponseModel
 import com.example.dariush.data.remote.WeatherRepository
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SplashViewModelTest {
 
-  @OptIn(ExperimentalCoroutinesApi::class)
   @get:Rule
   val mainCoroutineRule = MainCoroutineRule()
+
+  @get:Rule
+  val taskExecutor = InstantTaskExecutorRule()
 
   @MockK(relaxed = true)
   lateinit var mockWeatherRepository: WeatherRepository
@@ -35,9 +49,30 @@ class SplashViewModelTest {
   }
 
   @Test
-  fun `Given weather data is being fetched, When fetch is successful, Then navigate to the search screen`() {
-    TODO("unimplemented")
-  }
+  fun `Given weather data is being fetched, When fetch is successful, Then navigate to the search screen`() =
+    runTest {
+      val defaultLocation = "Istanbul"
+      val dummyResponse = WeatherResponseModel()
+      mockWeatherRepository.apply {
+        coEvery { fetchLocationData(defaultLocation) } coAnswers {
+          delay(2000)
+          Result.Success(dummyResponse)
+        }
+      }
+      val events = mutableListOf<SplashViewModel.Event>()
+      val vm = SplashViewModel(mockWeatherRepository)
+      val eventsJob = launch(UnconfinedTestDispatcher(testScheduler)) {
+        vm.eventsFlow.collect { event ->
+          events.add(event)
+        }
+      }
+
+      advanceTimeBy(2001)
+
+      vm.uiState.value?.defaultWeatherData shouldBe dummyResponse
+      events shouldContain SplashViewModel.Event.NavigateToSearchScreen
+      eventsJob.cancel()
+    }
 
   @Test
   fun `Given weather data is fetching, When fetch fails, Then show error and exit the app`() {
